1963562a5043ba5c7b2aba795a373ef96e56b784
MartvdZalm <martvanderzalm@gmail.com>
2025-06-06 00:53:06 +0200

    Added some more handy features

--- arg_parser.hpp
+++ arg_parser.hpp
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <string>
 #include <vector>
@@ -6,6 +6,9 @@
 #include <stdexcept>
 #include <functional>
 #include <variant>
+#include <optional>
+#include <algorithm>
+#include <cstdlib>
 
 namespace argparse
 {
@@ -17,21 +20,24 @@ namespace argparse
 
 	class Argument
 	{
+	public: enum class ArgType { INT, FLOAT, STRING, BOOL, AUTO };
+	private:
 		std::string name_;
 		std::vector<std::string> aliases_;
 		std::string help_;
 		bool required_ = false;
-		std::variant<int, float, std::string, bool> default_;
+		std::variant<int, float, std::string, bool> default_value_;
 		bool is_flag_ = false;
+		std::optional<int> min_value_;
+		std::optional<int> max_value_;
+		std::optional<std::string> env_var_;
+		std::vector<std::string> choices_;
+		ArgType type_ = ArgType::AUTO;
 
 		static std::string normalize_name(std::string name)
 		{
-			if (name.starts_with("--")) {
-				return name.substr(2);
-			}
-			if (name.starts_with("-")) {
-				return name.substr(1);
-			}
+			if (name.starts_with("--")) return name.substr(2);
+			if (name.starts_with("-")) return name.substr(1);
 			return name;
 		}
 
@@ -56,21 +62,126 @@ namespace argparse
 		const std::string& name() const { return name_; }
 		const std::vector<std::string>& aliases() const { return aliases_; }
 		const std::string& help() const { return help_; }
+		const ArgType type() const { return type_; }
 		bool is_flag() const { return is_flag_; }
-		const auto& default_value() const { return default_; }
+		const auto& default_value() const { return default_value_; }
+
+		std::optional<std::string> get_env_value() const
+		{
+			if (!env_var_) return std::nullopt;
+			char* buffer = nullptr;
+			size_t size = 0;
+			if (_dupenv_s(&buffer, &size, env_var_->c_str()) == 0 && buffer != nullptr) {
+				std::string result(buffer);
+				free(buffer);
+				return result;
+			}
+			return std::nullopt;
+		}
 
 		template<typename T>
 		Argument& default_value(T val)
 		{
-			default_ = val;
+			default_value_ = val;
 			return *this;
 		}
 
 		Argument& flag(bool set = true)
 		{
 			is_flag_ = set;
-			if (set) default_ = false;
+			if (set) default_value_ = false;
 			return *this;
+		}
+
+		Argument& min_value(int val)
+		{
+			min_value_ = val;
+			return *this;
+		}
+
+		Argument& max_value(int val)
+		{
+			max_value_ = val;
+			return *this;
+		}
+
+		Argument& env(const std::string& var_name)
+		{
+			env_var_ = var_name;
+			return *this;
+		}
+
+		Argument& choices(std::vector<std::string> options)
+		{
+			choices_ = std::move(options);
+			return *this;
+		}
+
+		Argument& type_int()
+		{
+			type_ = ArgType::INT;
+			if (std::holds_alternative<int>(default_value_)) return *this;
+			default_value_ = 0;
+			return *this;
+		}
+
+		Argument& type_float()
+		{
+			type_ = ArgType::FLOAT;
+			if (std::holds_alternative<float>(default_value_)) return *this;
+			default_value_ = 0.0f;
+			return *this;
+		}
+
+		Argument& type_string()
+		{
+			type_ = ArgType::STRING;
+			if (std::holds_alternative<std::string>(default_value_)) return *this;
+			default_value_ = std::string();
+			return *this;
+		}
+
+		Argument& type_bool()
+		{
+			type_ = ArgType::BOOL;
+			if (std::holds_alternative<bool>(default_value_)) return *this;
+			default_value_ = false;
+			return *this;
+		}
+
+		void validate(const std::string& value_str) const
+		{
+			if (type_ == ArgType::INT) {
+				if (value_str.empty()) throw ArgumentError("Missing integer value");
+				try {
+					int value = std::stoi(value_str);
+					if (min_value_ && value < *min_value_) {
+						throw ArgumentError("Value must be > " + std::to_string(*min_value_));
+					}
+					if (max_value_ && value > *max_value_) {
+						throw ArgumentError("Value must be < " + std::to_string(*max_value_));
+					}
+				}
+				catch (...) {
+					throw ArgumentError("Invalid integer value: " + value_str);
+				}
+			}
+			else if (!choices_.empty()) {
+				if (std::find(choices_.begin(), choices_.end(), value_str) == choices_.end()) {
+					throw ArgumentError("Invalid choice. Options: " + join_strings(choices_, ", "));
+				}
+			}
+		}
+
+	private:
+		static std::string join_strings(const std::vector<std::string>& vec, const std::string& delim)
+		{
+			std::string result;
+			for (size_t i = 0; i < vec.size(); ++i) {
+				if (i != 0) result += delim;
+				result += vec[i];
+			}
+			return result;
 		}
 	};
 
@@ -91,20 +202,22 @@ namespace argparse
 
 	class ArgumentParser
 	{
+		bool auto_help_ = true;
 		std::string prog_name_;
 		std::vector<Argument> args_;
-		std::unordered_map<std::string, std::string> arg_lookup_;
+		std::unordered_map<std::string, size_t> arg_index_;
 
 		void build_lookup()
 		{
-			arg_lookup_.clear();
-			for (const auto& arg : args_) {
-				arg_lookup_[arg.name()] = arg.name();
+			arg_index_.clear();
+			for (size_t i = 0; i < args_.size(); ++i) {
+				const auto& arg = args_[i];
+				arg_index_[arg.name()] = i;
 				for (const auto& alias : arg.aliases()) {
-					if (arg_lookup_.count(alias)) {
+					if (arg_index_.count(alias)) {
 						throw ArgumentError("Duplicate alias: -" + alias);
 					}
-					arg_lookup_[alias] = arg.name();
+					arg_index_[alias] = i;
 				}
 			}
 		}
@@ -129,7 +242,14 @@ namespace argparse
 		}
 
 	public:
+
 		explicit ArgumentParser(std::string prog_name) : prog_name_(std::move(prog_name)) {}
+
+		ArgumentParser& auto_help(bool enable)
+		{
+			auto_help_ = enable;
+			return *this;
+		}
 
 		Argument& add_argument(std::string name)
 		{
@@ -149,75 +269,111 @@ namespace argparse
 		ParsedArgs parse_args(int argc, char** argv)
 		{
 			build_lookup();
-
 			std::unordered_map<std::string, std::string> provided_args;
 			ParsedArgs result;
 
+			if (auto_help_) {
+				for (int i = 1; i < argc; ++i) {
+					if (std::string(argv[i]) == "--help") {
+						std::cout << help();
+						std::exit(0);
+					}
+				}
+			}
+
 			for (int i = 1; i < argc; ++i) {
 				std::string arg = argv[i];
-				std::string name;
-				std::string value;
+				std::string name, value;
 
 				if (arg.starts_with("--")) {
 					name = arg.substr(2);
+					if (!arg_index_.count(name)) {
+						throw ArgumentError("Unrecognized argument: --" + name);
+					}
 					if (i + 1 < argc && !std::string(argv[i + 1]).starts_with("-")) {
 						value = argv[++i];
+					}
+					else if (!arg_index_.count(name) || !args_[arg_index_.at(name)].is_flag()) {
+						throw ArgumentError("Missing value for --" + name);
 					}
 				}
 				else if (arg.starts_with("-")) {
 					name = arg.substr(1);
+					if (!arg_index_.count(name)) {
+						throw ArgumentError("Unrecognized argument: -" + name);
+					}
 					if (i + 1 < argc && !std::string(argv[i + 1]).starts_with("-")) {
 						value = argv[++i];
 					}
+					else if (!arg_index_.count(name) || !args_[arg_index_.at(name)].is_flag()) {
+						throw ArgumentError("Missing value for -" + name);
+					}
 				}
 
-				if (!name.empty()) {
-					if (!arg_lookup_.count(name)) {
-						throw ArgumentError("Unknown argument: " + arg);
-					}
-					provided_args[arg_lookup_.at(name)] = std::move(value);
+				if (!name.empty() && arg_index_.count(name)) {
+					provided_args[args_[arg_index_.at(name)].name()] = std::move(value);
 				}
 			}
 
 			for (const auto& arg : args_) {
 				const std::string& name = arg.name();
-
-				if (arg.is_required() && !provided_args.count(name)) {
-					throw ArgumentError("Missing required argument: --" + name);
-				}
+				result.values_[name] = arg.default_value();
 
 				if (provided_args.count(name)) {
 					const std::string& value_str = provided_args.at(name);
-
-					if (arg.is_flag()) {
-						result.values_[name] = true;
-					}
-					else {
-						try {
-							if (std::holds_alternative<int>(arg.default_value())) {
-								result.values_[name] = std::stoi(value_str);
-							}
-							else if (std::holds_alternative<float>(arg.default_value())) {
-								result.values_[name] = std::stof(value_str);
-							}
-							else if (std::holds_alternative<std::string>(arg.default_value())) {
-								result.values_[name] = value_str;
-							}
-							else if (std::holds_alternative<bool>(arg.default_value())) {
-								result.values_[name] = (value_str == "true" || value_str == "1");
-							}
-						}
-						catch (...) {
-							throw ArgumentError("Invalid value for argument: --" + name);
-						}
-					}
+					arg.validate(value_str);
+					result.values_[name] = convert_value(value_str, arg.type());
 				}
-				else {
-					result.values_[name] = arg.default_value();
+				else if (auto env_val = arg.get_env_value()) {
+					arg.validate(*env_val);
+					result.values_[name] = convert_value(*env_val, arg.type());
+				}
+				else if (arg.is_required()) {
+					throw ArgumentError("Missing required argument: --" + name);
 				}
 			}
 
 			return result;
+		}
+
+	private:
+		std::variant<int, float, std::string, bool> convert_value(const std::string& value_str, Argument::ArgType type)
+		{
+			try {
+				switch (type)
+				{
+					case Argument::ArgType::INT: return std::stoi(value_str);
+					case Argument::ArgType::FLOAT: return std::stof(value_str);
+					case Argument::ArgType::BOOL: return (value_str == "true" || value_str == "1");
+					case Argument::ArgType::STRING: return value_str;
+					case Argument::ArgType::AUTO: {
+						if (value_str == "true" || value_str == "false" ||
+							value_str == "1" || value_str == "0") {
+							return value_str == "true" || value_str == "1";
+						}
+
+						try {
+							size_t pos;
+							int i = std::stoi(value_str, &pos);
+							if (pos == value_str.size()) return i;
+						}
+						catch (...) {}
+
+						try {
+							size_t pos;
+							float f = std::stof(value_str, &pos);
+							if (pos == value_str.size()) return f;
+						}
+						catch (...) {}
+
+						return value_str;
+					}
+					default: return value_str;
+				}
+			}
+			catch (...) {
+				throw ArgumentError("Invalid value format");
+			}
 		}
 	};
 }
--- main.cpp
+++ main.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include "arg_parser.hpp"
 
-using namespace std;
-
 int main(int argc, char** argv)
 {
 	argparse::ArgumentParser parser("myapp");
-	parser.add_argument("count")
-		.help("Number of times")
-		.default_value(1)
-		.add_alias("c");
-	
-	parser.add_argument("verbose")
-		.add_alias("v")
-		.add_alias("debug")
-		.help("Enable verbose output")
-		.flag();
+	parser.add_argument("port")
+		.type_int()
+		.min_value(1024)
+		.max_value(49151)
+		.default_value(8080)
+		.help("Network port")
+		.add_alias("p");
+
+	parser.add_argument("color")
+		.type_string()
+		.choices({ "red", "blue", "green" })
+		.help("Choose a color")
+		.default_value("red");
 
 	try {
 		auto args = parser.parse_args(argc, argv);
-		int count = args.get<int>("count");
-		bool verbose = args.get<bool>("verbose");
+		int port = args.get<int>("port");
+		std::string color = args.get<std::string>("color");
+		std::string apiKey = args.get<std::string>("api-key");
 
-		cout << "Count: " << count << endl;
-		cout << "Verbose: " << boolalpha << verbose << endl;
+		std::cout << "Port: " << port << std::endl;
+		std::cout << "Color: " << color << std::endl;
 	}
 	catch (const argparse::ArgumentError& e) {
-		cerr << "Error: " << e.what() << "\n";
-		cerr << parser.help();
+		std::cerr << "Error: " << e.what() << "\n";
+		return 1;
 	}
-
 	return 0;
 }
